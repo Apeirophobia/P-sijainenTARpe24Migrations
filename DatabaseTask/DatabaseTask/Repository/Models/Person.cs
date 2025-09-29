610ec788b8d42968f941e55974769eb9669da902
acro <ervinpusijainen@gmail.com>
2025-09-29 10:36:43 +0300

    Database First Migration done

---
+++ DatabaseTask/DatabaseTask/Repository/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTask.Repository.Models;
+
+[Table("Person")]
+public partial class Person
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(20)]
+    public string? FirstName { get; set; }
+
+    [StringLength(20)]
+    public string? LastName { get; set; }
+
+    [StringLength(30)]
+    public string? Email { get; set; }
+}
---
+++ DatabaseTask/DatabaseTask/Repository/TestDBDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using DatabaseTask.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTask.Repository;
+
+public partial class TestDBDbContext : DbContext
+{
+    public TestDBDbContext()
+    {
+    }
+
+    public TestDBDbContext(DbContextOptions<TestDBDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TestDB;Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Person__3214EC07BE527C78");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
